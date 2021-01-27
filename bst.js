8c5343093113748b2061a7a01383ca49cadd1a1b
Ricardo Guerrero <richi_gro@hotmail.com>
2021-01-27 12:57:49 -0600

    boilerplate for binary search tree data structure

---
+++ bst.js
@@ -0,0 +1,14 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor(){
+    this.root = null;
+  }
+}
+
