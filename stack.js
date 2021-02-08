7672138b2f597bd1942bf5605aceb539c69e7c58
Ricardo Guerrero <richi_gro@hotmail.com>
2021-02-08 11:53:35 -0600

    added new method to stack class, called peek to be able to see last item in queue

--- stack.js
+++ stack.js
@@ -21,7 +21,12 @@ Stack.prototype.pop = function() {
 
 Stack.prototype.size = function() {
   return this.size;
-}
+};
+
+Stack.prototype.peek = function() {
+  // show the item on top of the stack the last one addded in
+  return this.storage[this.size -1];
+};
 
 var myStack = new Stack();
 myStack.push(5);
@@ -31,19 +36,21 @@ myStack.push(3);
 myStack.push(1);
 console.log(myStack);
 ///delete last item put in 
-console.log(myStack.pop());
+console.log(myStack.pop()); // 1
 console.log(myStack);
 
-console.log(myStack.pop());
-console.log(myStack);
+console.log("peeking at: ", myStack.peek());
 
-console.log(myStack.pop());
-console.log(myStack);
+// console.log(myStack.pop());
+// console.log(myStack);
 
-console.log(myStack.pop());
-console.log(myStack);
+// console.log(myStack.pop());
+// console.log(myStack);
 
-console.log(myStack.pop());
-console.log(myStack);
+// console.log(myStack.pop());
+// console.log(myStack);
 
-console.log(myStack.pop());
+// console.log(myStack.pop());
+// console.log(myStack);
+
+// console.log(myStack.pop());
